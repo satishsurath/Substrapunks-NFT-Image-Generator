fec33194538b9d2aafb002db54e1aa5913e15f56
Satish Surath <s@n-m.co>
2022-07-12 10:35:40 -0400

    npx hardhat compile
    Signed-off-by: Satish Surath <s@n-m.co>

--- nft/hardhat.config.js
+++ nft/hardhat.config.js
@@ -1,4 +1,33 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+//module.exports = {
+//  solidity: "0.8.0",
+//};
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: "0.8.9",
-};
+   solidity: {
+    compilers: [
+      {
+        version: "0.8.1"
+      },
+      {
+        version: "0.8.0"
+      },
+      {
+        version: "0.7.3"
+      }
+    ]
+  },
+   defaultNetwork: "rinkeby",
+   networks: {
+      hardhat: {},
+      rinkeby: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
